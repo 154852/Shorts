6c04869ea1f790d3b4d68187f81d08efaec34253
Mindstormer <louisemileploix@icloud.com>
2018-11-27 07:53:57 +0000

    Create client.js

---
+++ TypeRacer/client.js
@@ -0,0 +1,11 @@
+const __interval = setInterval(() => {
+    if (document.querySelector('.popupContent .time').innerText == ':00') {
+        setTimeout(() => {
+            y = new XMLHttpRequest();
+            y.open('GET', 'http://localhost:8080/' + document.querySelector('.inputPanel span').parentElement.innerText);
+            y.send();
+        }, 550);
+
+        window.clearInterval(__interval);
+    }
+})
